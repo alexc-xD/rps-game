5539449ccaa3efcf81a470f05093660cf740900b
alexc-xD <alexandercacorrea@gmail.com>
2022-06-26 16:27:48 -0400

    edited html and js added ui
    html: added button elements and score
    js: added listeners to call playRound function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let playerScore = 0
 let computerScore = 0
 
+const score = document.querySelector('#score');
+
 function computerPlay() {
     // randomly return Rock Paper or Scissors
     random = Math.floor(Math.random() * 3);
@@ -19,7 +21,8 @@ function playRound(playerSelection, computerSelection) {
     //prompt playerSelection
     //call computerPlay to get computerSelection
     //compare the two and determine the winner
-    playerSelection = prompt("Enter 'Rock' 'Paper' or 'Scissors'");
+    //    playerSelection = prompt("Enter 'Rock' 'Paper' or 'Scissors'");
+    //playerSelection = arguments[0];
     computerSelection = computerPlay();
 
     console.log("Your " + playerSelection + " Vs. Opponent's " + computerSelection)
@@ -28,18 +31,39 @@ function playRound(playerSelection, computerSelection) {
         || (playerSelection == 'Paper') && (computerSelection == 'Rock')
         || (playerSelection == 'Scissors') && (computerSelection == 'Paper')) {
         playerScore++;
-        return "You Win! Score: You " + playerScore + " Cpu " + computerScore
+        console.log("You Win! Score: You " + playerScore + " Cpu " + computerScore)
+        score.textContent = "Score: You " + playerScore + " Cpu " + computerScore
 
     } else if (playerSelection == 'Rock' && (computerSelection == 'Paper')
         || (playerSelection == 'Paper') && (computerSelection == 'Scissors')
         || (playerSelection == 'Scissors') && (computerSelection == 'Rock')) {
         computerScore++;
-        return "You Lose! Score: You " + playerScore + " Cpu " + computerScore
+        console.log("You Lose! Score: You " + playerScore + " Cpu " + computerScore)
+        score.textContent = "Score: You " + playerScore + " Cpu " + computerScore
     } else if (playerSelection == 'Rock' && (computerSelection == 'Rock')
         || (playerSelection == 'Paper') && (computerSelection == 'Paper')
         || (playerSelection == 'Scissors') && (computerSelection == 'Scissors')) {
-        return "You Tie! Score: You " + playerScore + " Cpu " + computerScore
+        console.log("You Tie! Score: You " + playerScore + " Cpu " + computerScore)
+        score.textContent = "Score: You " + playerScore + " Cpu " + computerScore
     } else {
-        return "Invalid Input!"
+        console.log("Invalid Input!")
     }
 }
+
+const rock = document.querySelector('#rock');
+
+rock.addEventListener('click', () => {
+    playRound('Rock');
+});
+
+const paper = document.querySelector('#paper');
+
+paper.addEventListener('click', () => {
+    playRound('Paper');
+});
+
+const scissors = document.querySelector('#scissors');
+
+scissors.addEventListener('click', () => {
+    playRound('Scissors');
+});
